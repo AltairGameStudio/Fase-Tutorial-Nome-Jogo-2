5ca5efb33c86c88c637b3e8f2d4bc0d447c656b1
Davi Piske <davi.f.piske@gmail.com>
2026-03-28 15:43:17 -0300

    movimentacao da nave + atira

--- Scripts/Entities/EnemyShip.gd
+++ Scripts/Entities/EnemyShip.gd
@@ -1,19 +1,45 @@
 class_name EnemyShip
 extends CharacterBody2D
+var Placeholder = 1
 
 ## NAVE INIMIGA
 @export var max_health: int = 100
 @export var movement_speed: float = 150.0
-
+@export var tiro_cooldown: float = 0.5
 var current_health: int
+
+var podeatirar= true
+
+# valor placeholder
+@export var bullet_damage = 10
+@export var bullet_speed = 2
 
 func _ready() -> void:
 	current_health = max_health
 
 ## Lógica de movimentação lateral automática
 func _physics_process(delta: float) -> void:
-	pass
+	#vai pro canto e volta pro outro
+	if position.x>150: movement_speed= -3
+	if position.x<0: movement_speed= 3
+	position.x += movement_speed+delta
 
 ## Processa o dano recebido pelos ataques dos aliens
-func take_damage(amount: int) -> void:
-	pass
+func take_damage(_bullet) -> void:
+	current_health = current_health - bullet_damage
+
+@export var bullet_scene = preload("res://Scenes/Entities/talvezissodetectecolisao.tscn")
+
+func _process(_delta): 
+	#cooldown de tiro
+	if podeatirar:
+		shoot()
+		podeatirar = false
+		await get_tree().create_timer(tiro_cooldown).timeout 
+		podeatirar = true
+
+func shoot():
+	#fazer tiro spawnar
+	var bullet = bullet_scene.instantiate()
+	get_parent().add_child(bullet)
+	bullet.position = position 
